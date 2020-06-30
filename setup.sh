0fc15546b3844e4e6b9c8ea8e7a1980554767763
Marcel Nguyen <43071189+MVanNguyen@users.noreply.github.com>
2020-06-30 11:21:02 -0400

    Updating setup.sh file
    Took out unnecessary comments/lines and placed sed commands with config files.

--- setup.sh
+++ setup.sh
@@ -11,6 +11,7 @@ sudo apt install dnsmasq -y
 
 # Copy the DNS Config File
 echo "Configuring DNS"
+#Changing code for DNS configuration
 sudo cp -fr dnsmasq.conf /etc/dnsmasq.conf
 
 # Resstart DNS Server
@@ -23,8 +24,8 @@ sudo apt-get install nut nut-client nut-server -y
 
 #The path to all NUT files on PI  --> /etc/nut/
 echo "Configuring ups.conf"
+#Inputting the code for UPS
 sudo cp -fr ups.conf /etc/nut/ups.conf
-sudo sed -i "s/PutUPSNameHere/$LocationName/g" /etc/nut/ups.conf
 
 
 echo "Configuring upsd.conf"
@@ -32,19 +33,27 @@ sudo sed -i 's/# LISTEN 127.0.0.1 3493/LISTEN 127.0.0.1 3493/g' /etc/nut/upsd.co
 
 
 echo "Configuring upsd.users"
+#inputting the code to manage users
 sudo cp -fr upsd.users /etc/nut/upsd.users
-sudo sed -i "s/PutUPSNameHere/$LocationName/g" /etc/nut/upsd.users
-
 
 echo "Configuring nut.conf"
 sudo sed -i 's/MODE=none/MODE=standalone/g' /etc/nut/nut.conf
 
 
 echo "Configuring upsmon.conf"
+#Inputting the code for UPS monitoring
 sudo cp -fr upsmon.conf /etc/nut/upsmon.conf
+
+#???
+#echo "Creating script to send curl command"
+#curl  --request POST 'https://prod-28.eastus2.logic.azure.com:443/workflows/42517d4652c9446d8808fe0665c76088/triggers/request/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Frequest%2Frun&sv=1.0&sig=oqY1WrPZX1_xK23Tj0h4lthuYT0vltjyW8QyRmou_GU' --header 'Content-Type: application/json' --data-raw '{ "location": "20171-00" }'
+
+
+#NOTE = PutUPSNameHere
+
+sudo sed -i "s/PutUPSNameHere/$LocationName/g" /etc/nut/ups.conf
+sudo sed -i "s/PutUPSNameHere/$LocationName/g" /etc/nut/upsd.users
 sudo sed -i "s/PutUPSNameHere/$LocationName/g" /etc/nut/upsmon.conf
-
-
 
 echo "Testing UPS driver"
 sudo upsdrvctl start
