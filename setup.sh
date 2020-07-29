6b562a3783f7379dd7b4dcba4eec9db59c5c8bd0
Marcel Nguyen <43071189+MVanNguyen@users.noreply.github.com>
2020-07-29 14:36:45 -0400

    setup.sh
    Put some *s next to the echo statements to make it more visible when the script is being run.

--- setup.sh
+++ setup.sh
@@ -1,50 +1,48 @@
 #!/bin/bash
 LocationName='20171-00'
-echo""
 # Update Packages
 echo ""
-echo "Updating Raspberry Pi"
-sudo apt update echo ""
-sudo apt upgrade
+echo "****Updating Raspberry Pi****"
+sudo apt update # && sudo apt-get -y upgrade
 echo ""
 # Install DNS Masq
-echo "Installing DNS"
+echo "****Installing DNS****"
 sudo apt install dnsmasq -y
 echo ""
 # Copy the DNS Config File
-echo "Configuring DNS"
+echo "****Configuring DNS****"
 sudo cp -fr dnsmasq.conf /etc/dnsmasq.conf
 echo ""
 # Resstart DNS Server
-echo "Restarting DNS"
+echo "****Restarting DNS****"
 sudo systemctl restart dnsmasq
 echo ""
-echo "Installing NUT-CLIENT & NUT-SERVER"
+echo "****Installing NUT-CLIENT & NUT-SERVER****"
 sudo apt-get install nut nut-client nut-server -y
 echo ""
 #The path to all NUT files on PI  --> /etc/nut/
-echo "Configuring ups.conf"
+echo "****Configuring ups.conf****"
 sudo cp -fr ups.conf /etc/nut/ups.conf
 sudo sed -i "s/PutUPSNameHere/$LocationName/g" /etc/nut/ups.conf
 echo ""
-echo "Configuring upsd.conf"
+echo "****Configuring upsd.conf****"
 sudo sed -i 's/# LISTEN 127.0.0.1 3493/LISTEN 127.0.0.1 3493/g' /etc/nut/upsd.conf
 echo ""
-echo "Configuring upsd.users"
+echo "****Configuring upsd.users****"
 sudo cp -fr upsd.users /etc/nut/upsd.users
 sudo sed -i "s/PutUPSNameHere/$LocationName/g" /etc/nut/upsd.users
 echo ""
-echo "Configuring nut.conf"
+echo "****Configuring nut.conf****"
 sudo sed -i 's/MODE=none/MODE=standalone/g' /etc/nut/nut.conf
 echo ""
-echo "Configuring upsmon.conf"
+echo "****Configuring upsmon.conf****"
 sudo cp -fr upsmon.conf /etc/nut/upsmon.conf
 sudo sed -i "s/PutUPSNameHere/$LocationName/g" /etc/nut/upsmon.conf
 echo ""
-echo "Testing UPS driver"
+echo "****Testing UPS driver****"
 sudo upsdrvctl start
 echo ""
-echo "Starting NUT-server"
+echo "****Starting NUT-server****"
 sudo service nut-server start
-echo "Starting NUT-client"
+echo "****Starting NUT-client****"
 sudo service nut-client start
